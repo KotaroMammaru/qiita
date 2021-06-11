f64c638b7cf2fbd4d13543c61c9937ca0047ecd4
Kotaro Mammaru <kotaro.112358@icloud.com>
2021-06-11 19:42:58 +0900

    'fis

--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import axios from "axios"
+import './index.css'
 
+
+function Popup (props) {
+    return (
+        <div className="popup">
+            <h1>{props.text}</h1>
+            <button onClick={props.closePopup}>close</button>
+        </div>
+    )
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            postsList: [],
+            pops: Array(3).fill(false)
+        };
+    }
+    renderImageList(list) {
+        this.renderaxios();
+        const posts = list.map((item, index) => {
+            return (
+                <li className="item" key={index}>
+                    <a href={item.url}>{item.title}</a>
+                    {this.state.pops[index] ?
+                        null
+                        :<button onClick={this.togglePopup(index)}>show popup</button>
+                    }
+                    {this.state.pops[index] ?
+                        <Popup text={item.body} closePopup={this.togglePopup(index)} />
+                        :null
+                    }
+                </li>
+            )
+        });
+        return posts
+    }
+    togglePopup(i) {
+        const pops = this.state.pops.slice()
+        pops[i] = ! pops[i]
+        this.setState({
+            pops: pops
+        })
+    }
+    
+    renderaxios(){
+        const url = 'https://qiita.com/api/v2/items?page=1&per_page=3';
+       axios
+        .get(url, {
+            headers: {
+                Authorization: "Bearer b73157fc33f765e8d39ac7f44c263651f22b3e00"
+            }
+        }).then((res) => {
+            this.setState({postsList: res.data})
+        } ).catch(console.error);
+    }
+    
+    render() {
+        const url = 'https://qiita.com/api/v2/items?page=1&per_page=3';
+       axios
+        .get(url, {
+            headers: {
+                Authorization: "Bearer b73157fc33f765e8d39ac7f44c263651f22b3e00"
+            }
+        }).then((res) => {
+            this.setState({postsList: res.data})
+        } ).catch(console.error);
+        return (
+            <div className="whole">
+                <header>
+                Qiita
+                </header>
+                <div className="link">
+                    <ul>{this.renderImageList(this.state.postsList)}</ul>
+                </div>
+            </div>
+            
+        );
+    }
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
